ff0c15b155fc00b409ba954ad3e1e96bfcab2e77
ryan-christopher <rechristopher99@gmail.com>
2023-04-21 00:09:32 -0400

    Week 2 Project for Alchemy University

--- client/index.js
+++ client/index.js
@@ -7,8 +7,20 @@ const serverUrl = 'http://localhost:1225';
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
 
+  let name = "Din Djarin";
+
+
+  const merkleNames = new MerkleTree(niceList)
+  console.log(merkleNames.getRoot())
+
+  const nameIndex = niceList.indexOf(name)
+
+  const proof = merkleNames.getProof(nameIndex)
+
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    name: name,
+    proof: proof
   });
 
   console.log({ gift });
--- server/index.js
+++ server/index.js
@@ -8,15 +8,18 @@ app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+
+const MERKLE_ROOT = '4dacd40c754b4e10f89d2d27a231bd61030549739cf91f83c8e6b19a5b6a9f08';
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
   const body = req.body;
+  const name = body.name;
+  const proof = body.proof;
 
   // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT)
+  if (isInTheList) {
     res.send("You got a toy robot!");
   }
   else {
